71f12c72ff01a57de2bfeb07f16e30a4f355d799
souravpal01 <souravpal7217@gmail.com>
2022-12-10 23:49:15 +0530

    added openapi

---
+++ src/main/java/com/sourav/calculator/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.sourav.calculator.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Tag;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build()
+                .tags(new Tag("user-controller", "These endpoints are used to manage the users details.", 1));
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("API Documentation")
+                .description("API Documentation Using Swagger2 on Spring Boot Project")
+                .version("1.0")
+                .build();
+    }
+}
