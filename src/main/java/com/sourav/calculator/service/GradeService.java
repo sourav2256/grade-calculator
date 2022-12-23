72b24289fe562add7efffd2dd63bbf8cfa6f8d2b
souravpal01 <souravpal7217@gmail.com>
2022-12-23 23:21:50 +0530

    code added

--- src/main/java/com/sourav/calculator/service/GradeService.java
+++ src/main/java/com/sourav/calculator/service/GradeService.java
@@ -4,13 +4,14 @@ import com.sourav.calculator.model.Grade;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface GradeService {
     List<Grade> getGrades();
 
-    Grade findBy(String name);
+    Optional<Grade> findBy(String name);
 
-    String handleSubmit(Grade grade);
+    Grade handleSubmit(Grade grade);
 
     void updateGrade(String id, Grade grade);
 }
