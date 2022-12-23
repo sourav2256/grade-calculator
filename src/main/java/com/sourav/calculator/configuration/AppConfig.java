15575b0ac07fc94cbef3f858f259356c80615986
souravpal01 <souravpal7217@gmail.com>
2022-12-23 23:52:00 +0530

    code added

--- src/main/java/com/sourav/calculator/configuration/AppConfig.java
+++ src/main/java/com/sourav/calculator/configuration/AppConfig.java
@@ -1,14 +1,61 @@
-//package com.sourav.calculator.configuration;
-//
-//import com.sourav.calculator.Repository.GradeRepo;
-//import com.sourav.calculator.service.GradeService;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class AppConfig {
+package com.sourav.calculator.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+class BasicAuthWebSecurityConfiguration
+{
+    //@Autowired
+    //private AppBasicAuthenticationEntryPoint authenticationEntryPoint;
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/grades").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                //.authenticationEntryPoint(authenticationEntryPoint);
+        return http.build();
+    }
 //    @Bean
-//    public GradeService gradeService() {
-//        return new GradeService();
+//    public InMemoryUserDetailsManager userDetailsService() {
+//        UserDetails user = User
+//                .withUsername("user")
+//                .password(passwordEncoder().encode("password"))
+//                .roles("USER_ROLE")
+//                .build();
+//        return new InMemoryUserDetailsManager(user);
 //    }
-//}
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(8);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService1() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin-pass"))
+                .roles("ADMIN")
+                .build();
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("user-pass"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(admin);
+    }
+}
